be65aaf1979e0b9aa12bcad7a9d546bb6a3df116
lvlvSSS <nelson.lv.007@outlook.com>
2023-01-13 10:26:11 +0800

    add ip tools

--- net/net_helper.go
+++ net/net_helper.go
@@ -1,10 +1,73 @@
 package net
 
-import "regexp"
+import (
+	"fmt"
+	"net"
+	"regexp"
+)
 
 const IpRegex = `^([1-9]|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])(\.(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){3}$`
 
 func IsIP(address string) bool {
 	compile := regexp.MustCompile(IpRegex)
 	return compile.MatchString(address)
+}
+
+func GetLocalIP() string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+		if iface.Flags&net.FlagBroadcast == 0 {
+			continue
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+		for _, address := range addrs {
+			var ip net.IP
+			switch v := address.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip.DefaultMask() == nil {
+				continue
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue
+			}
+			return ip.String()
+		}
+
+	}
+	return ""
+}
+
+func GetOutBoundIP() (ip string, err error) {
+	conn, err := net.Dial("udp", "8.8.8.8:53")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	ip, _, err = net.SplitHostPort(localAddr.String())
+	if err != nil {
+		return
+	}
+	return
 }
