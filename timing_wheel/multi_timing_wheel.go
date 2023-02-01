bada09ef4a58947ae45d6bc1306303c622d904c7
lvlvSSS <nelson.lv.007@outlook.com>
2023-02-01 15:23:10 +0800

    Append method in Slot finished

--- timing_wheel/multi_timing_wheel.go
+++ timing_wheel/multi_timing_wheel.go
@@ -1,25 +1,158 @@
 package timing_wheel
 
-type jiffy int64
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+	"unsafe"
+)
+
+var Unmodified = errors.New("state in timing wheel can't be modified now")
+
+type state uint32
+
+const (
+	unstarted state = 1 << iota
+	running
+	finished
+)
 
 type TimingWheel struct {
-	bucket_L1 [256]Node
-	bucket_L2 [64]Node
-	bucket_L3 [64]Node
-	bucket_L4 [64]Node
-	bucket_L5 [64]Node
+	bucket *Bucket
 
-	duration jiffy
-	next     *TimingWheel
+	jiffy time.Duration
+	next  *TimingWheel
+
+	state state
+	lock  sync.Mutex
+}
+
+func (wheel *TimingWheel) DefineJiffy(dur time.Duration) error {
+	if state(atomic.LoadUint32((*uint32)(&wheel.state))) == unstarted {
+		wheel.lock.Lock()
+		defer wheel.lock.Unlock()
+		if state(atomic.LoadUint32((*uint32)(&wheel.state))) == unstarted {
+			wheel.jiffy = dur
+		}
+		return nil
+	}
+	return Unmodified
+}
+
+func (wheel *TimingWheel) Run() error {
+	for {
+
+		time.Sleep(wheel.jiffy / 4)
+	}
+	return nil
 }
 
 type Node struct {
-	next, prev *Node
-	do         func(callback func())
+	index int
+	do    func()
+
+	// depth, parent, left, right, next - all used to iterate the tree that built by Nodes.
+	depth       int32 // only the root node record the value.
+	parent      *Node
+	left, right *Node
+	next        *Node
 }
 
-// level - like linux's timing wheel, l1 has 256 jiffies, l2~ln has 64 jiffies.
-func level(duration uint64) (lvl, lvl_idx, idx int) {
+func (node *Node) String() string {
+	return fmt.Sprintf("Node{index[%d], depth[%d], next[%v]}", node.index, node.depth, node.next)
+}
+
+type Bucket struct {
+	slots []Slot
+	next  *Bucket
+}
+
+// Slot - AVL tree
+type Slot struct {
+	root    *Node
+	current *Node // record the new node should be added after the current node.
+}
+
+// Append - add node to the end of Slot without lock.
+func (slot *Slot) Append(node *Node) {
+	// if the root is nil, then record the node as root.
+	if slot.root == nil {
+		if atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(&slot.root)), nil, unsafe.Pointer(node)) {
+			atomic.AddInt32(&node.depth, 1)
+			atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&slot.current)), unsafe.Pointer(node))
+			return
+		}
+	}
+	for {
+		cur := (*Node)(atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&slot.current))))
+		if cur == nil {
+			continue
+		}
+		if cur != slot.root && cur.parent.left == cur {
+			if slot.leftAppend(cur, node) {
+				break
+			}
+		} else {
+			if slot.rightAppend(cur, node) {
+				break
+			}
+		}
+	}
+
+}
+
+// leftAppend - current is the left leaf of the parent.
+func (slot *Slot) leftAppend(cur, node *Node) bool {
+	if atomic.CompareAndSwapPointer(
+		(*unsafe.Pointer)(unsafe.Pointer(&cur.next)), nil, unsafe.Pointer(node),
+	) {
+		cur.parent.right = node
+		node.parent = cur.parent
+		atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&slot.current)), unsafe.Pointer(node))
+		return true
+	}
+	return false
+}
+
+// rightAppend - current is the right leaf of the parent.
+func (slot *Slot) rightAppend(cur, node *Node) bool {
+	// check whether the current node is the end of the layer of the tree.
+	if cur == slot.root || cur.parent.next == nil {
+		// the node added to the first node of the layer of the tree
+		left := slot.root
+		right := slot.root
+		for {
+			if right == cur {
+				break
+			}
+			left = left.left
+			right = right.right
+		}
+		node.parent = left
+		if atomic.CompareAndSwapPointer(
+			(*unsafe.Pointer)(unsafe.Pointer(&left.left)), nil, unsafe.Pointer(node),
+		) {
+			atomic.AddInt32(&slot.root.depth, 1)
+			atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&slot.current)), unsafe.Pointer(node))
+			return true
+		}
+	} else {
+		if atomic.CompareAndSwapPointer(
+			(*unsafe.Pointer)(unsafe.Pointer(&cur.next)), nil, unsafe.Pointer(node),
+		) {
+			cur.parent.next.left = node
+			node.parent = cur.parent.next
+			atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&slot.current)), unsafe.Pointer(node))
+			return true
+		}
+	}
+	return false
+}
+
+// location - like linux's timing wheel, l1 has 256 slots, l2~ln has 64 slots.
+func location(duration uint64) (lvl, lvl_idx, idx int) {
 	const L1 = 0xff
 	const Ln = 0x3f
 	idx = int(duration & L1)
@@ -28,7 +161,7 @@ func level(duration uint64) (lvl, lvl_idx, idx int) {
 		lvl_idx = int(cur&Ln) - 1
 		lvl++
 	}
-	/*	cur := duration >> 8
+	/*	cur := jiffy >> 8
 		for {
 			if cur&Ln <= 0 && cur < Ln {
 				return
--- timing_wheel/multi_timing_wheel_test.go
+++ timing_wheel/multi_timing_wheel_test.go
@@ -1,20 +1,111 @@
 package timing_wheel
 
-import "testing"
+import (
+	"container/list"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"unsafe"
+)
 
 func TestLevel(t *testing.T) {
-	lvl, lvl_idx, idx := level(232)
-	t.Logf("level : %d, idx in level : %d, idx: %d", lvl, lvl_idx, idx)
+	lvl, lvl_idx, idx := location(232)
+	t.Logf("location : %d, idx in location : %d, idx: %d", lvl, lvl_idx, idx)
 
-	lvl, lvl_idx, idx = level(256)
-	t.Logf("level : %d, idx in level : %d, idx: %d", lvl, lvl_idx, idx)
+	lvl, lvl_idx, idx = location(256)
+	t.Logf("location : %d, idx in location : %d, idx: %d", lvl, lvl_idx, idx)
 
-	lvl, lvl_idx, idx = level(635)
-	t.Logf("level : %d, idx in level : %d, idx: %d", lvl, lvl_idx, idx)
+	lvl, lvl_idx, idx = location(635)
+	t.Logf("location : %d, idx in location : %d, idx: %d", lvl, lvl_idx, idx)
 
-	lvl, lvl_idx, idx = level(1048790)
-	t.Logf("level : %d, idx in level : %d, idx: %d", lvl, lvl_idx, idx)
+	lvl, lvl_idx, idx = location(1048790)
+	t.Logf("location : %d, idx in location : %d, idx: %d", lvl, lvl_idx, idx)
 
-	lvl, lvl_idx, idx = level(4194451)
-	t.Logf("level : %d, idx in level : %d, idx: %d", lvl, lvl_idx, idx)
+	lvl, lvl_idx, idx = location(4194451)
+	t.Logf("location : %d, idx in location : %d, idx: %d", lvl, lvl_idx, idx)
+}
+
+func TestPointer(t *testing.T) {
+	/*empty := &Node{
+		index: -1,
+	}*/
+	node := &Node{
+		index: 1,
+		do:    nil,
+		next:  nil,
+		//prev:  nil,
+	}
+	nextNode := &Node{
+		index: 10,
+	}
+	pointer := (*unsafe.Pointer)(unsafe.Pointer(&node.next))
+	t.Logf("next pointer : %v", pointer)
+	//t.Logf("equals : %v", pointer == unsafe.Pointer(empty))
+	swapped := atomic.CompareAndSwapPointer(pointer, nil, unsafe.Pointer(nextNode))
+	t.Logf("swap : %v", swapped)
+	if swapped {
+		t.Logf("new next node : %v", *node.next)
+	}
+	//atomic.StorePointer(&pointer, unsafe.Pointer(nextNode))
+	//t.Logf("new next node : %v", *node.next)
+
+}
+
+func TestSlot_Append(t *testing.T) {
+	slot := &Slot{
+		root:    nil,
+		current: nil,
+	}
+	starting := sync.WaitGroup{}
+	starting.Add(1)
+	all := sync.WaitGroup{}
+	for i := 1; i <= 100; i++ {
+		node := &Node{
+			index:  i,
+			do:     nil,
+			depth:  0,
+			parent: nil,
+			left:   nil,
+			right:  nil,
+			next:   nil,
+		}
+		node.do = func() {
+			t.Logf("index[%v], depth[%v], next[%v]", node.index, node.depth, *node.next)
+		}
+		all.Add(1)
+		go func() {
+			starting.Wait()
+			slot.Append(node)
+			all.Done()
+		}()
+	}
+	starting.Done()
+	all.Wait()
+
+	// print the slot
+	set := make(map[int]interface{}, 100)
+	l := list.New()
+	l.PushBack(slot.root)
+	for {
+		front := l.Front()
+		if front == nil {
+			break
+		}
+		l.Remove(front)
+		ele, _ := front.Value.(*Node)
+		t.Logf("element : %s", ele)
+		set[ele.index] = nil
+		if ele.left != nil {
+			l.PushBack(ele.left)
+		}
+		if ele.right != nil {
+			l.PushBack(ele.right)
+		}
+	}
+
+	if len(set) != 100 {
+		t.Logf("FAILED")
+	} else {
+		t.Logf("SUCCESS")
+	}
 }
